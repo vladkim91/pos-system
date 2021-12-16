c0fe30b2be1796a163336b77310a1eba03c62ee7
vladkim91 <vkcoding21@gmail.com>
2021-12-16 14:35:00 -0800

    catigories renders from DB

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,17 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     const getCategories = async () => {
       const res = await axios.get(`http://localhost:3001/api/ui/categories`);
-      const categories = res.data.category
-      console.log(categories)
+      const categories = res.data.category;
+
+      setCategories(categories);
     };
     getCategories();
   }, []);
 
-  return <div className="categories"></div>;
+  return (
+    <div className="categories">
+     
+        {categories.map((e,i) => {
+           return <button key={i}className="category">
+           {e.name}
+          </button>
+        })}
+      
+    </div>
+  );
 };
 
 export default Categories;
