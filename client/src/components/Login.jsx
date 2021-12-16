44b280c828c8aae64f8dc1962229748561b3770c
vladkim91 <vkcoding21@gmail.com>
2021-12-16 11:07:47 -0800

    login ui rendered based on access variable

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,38 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import DigitButton from './DigitButton';
 
-const Login = () => {
-  const [passcode, setPasscode] = useState('')
+const Login = ({clickHandler, checkPasscodeHandler, passcode}) => {
+  
   const buttons = [];
   for (let i = 0; i < 10; i++) {
     buttons.push(i);
   }
-
-  const clickHandler = (e) => {
-    let tempPasscode = passcode
-    tempPasscode = passcode + e.target.value 
-    setPasscode(tempPasscode)
-   
-  };
-
-
-  const checkPasscodeHandler = (e) => {
-    e.preventDefault()
-    console.log(passcode)
-  }
-
+  
 
   return (
-    <div className="login">
+
+    <div className="login-ui">
+      <h4>Use your passcode to login</h4>
       <div className="user-id">{passcode}</div>
       <div className="buttons">
         <div className="digit button">
           {buttons.map((e, i) => (
             <DigitButton value={e} key={i} clickHandler={clickHandler} />
           ))}
-          
         </div>
-        <button onClick={checkPasscodeHandler} className="button enter">Enter</button>
+        <button onClick={checkPasscodeHandler} className="button enter">
+          Enter
+        </button>
       </div>
     </div>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from '../components/Login';
 import About from '../pages/About';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 const Home = () => {
+  const [access, setAccess] = useState(false);
+  const [passcode, setPasscode] = useState('');
+
+  const clickHandler = (e) => {
+    let tempPasscode = passcode;
+    tempPasscode = passcode + e.target.value;
+    setPasscode(tempPasscode);
+  };
+
+  const checkPasscodeHandler = (e) => {
+    e.preventDefault();
+    if (passcode === '1111') {
+      setAccess(true);
+    }
+    setPasscode('');
+
+    console.log(passcode, access);
+  };
+
   return (
     <div className="landing-page">
-      This is a login page
-      <Route exact path="/" component={Login} />
+      <h1>Mahalo</h1>
+
+      {!access ? (
+        <Route
+          exact
+          path="/"
+          component={(props) => (
+            <Login
+              {...props}
+              clickHandler={clickHandler}
+              checkPasscodeHandler={checkPasscodeHandler}
+              passcode={passcode}
+            />
+          )}
+        />
+      ) : null}
+
       <Route path="/about" component={About} />
     </div>
   );
